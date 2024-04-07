46908fdf79c1a2bcf406e0b628fcacc883b7405d
Sumant Shah <sumant.shah@gigacars.com>
2024-04-07 15:03:05 +0530

    login api

--- modal/userSchema.js
+++ modal/userSchema.js
@@ -71,6 +71,18 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
+// we are generating token
+userSchema.methods.generateAuthToken = async function () {
+    try{
+        let token = jwt.sign({_id: this._id}, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token: token});
+        await this.save();
+        return token;
+    }catch(err) {
+        console.log(err);
+    }
+}
+
 
 const User = mongoose.model("USER", userSchema);
 
--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../modal/userSchema');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 require('../db/conn');
 
 
@@ -10,21 +11,21 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/register', async(req, res) => {
+router.post('/register', async (req, res) => {
     const { name, email, phone, work, password, cpassword } = req.body;
 
-    if(!name || !email || !phone || !work || !password || !cpassword) {
-        return res.status(422).json({error: "plz fill the field properly"});
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "plz fill the field properly" });
     }
 
-    try{
-        const userExist = await User.findOne({email: email});
+    try {
+        const userExist = await User.findOne({ email: email });
 
-        if(userExist){
-            return res.status(422).json({ error: "Email already exist "});
-        }else if(password != cpassword){
-            return res.status(422).json({error: "password is not matching"});
-        }else {
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exist " });
+        } else if (password != cpassword) {
+            return res.status(422).json({ error: "password is not matching" });
+        } else {
             const user = new User({
                 name,
                 email,
@@ -36,7 +37,7 @@ router.post('/register', async(req, res) => {
             await user.save();
             res.status(201).json({ message: "User registered successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         console.log(err);
     }
 });
@@ -44,9 +45,44 @@ router.post('/register', async(req, res) => {
 
 //login route
 router.post('/signin', async (req, res) => {
-    try{
+    try {
         const { email, password } = req.body;
-    } catch(err) {
+        if (!email || !password) {
+            return res.status(400).json({ error: "Plz fill the data" });
+        }
+
+        const userLogin = await User.findOne({ email: email });
+
+        // console.log(userLogin);
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(password, userLogin.password);
+            // console.log(isMatch);
+
+            const token = await userLogin.generateAuthToken();
+            // console.log(token);
+
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
+            });
+
+            if (!isMatch) {
+                res.status(400).json({ error: "invalid credentials" })
+            } else {
+                res.json({
+                    message: "User login successfully",
+                    userInfo: {
+                        name: userLogin.name,
+                        email: userLogin.email,
+                        phone: userLogin.phone
+                    }
+                })
+            }
+
+        } else {
+            res.status(400).json({error: "Invalid Credentials"})
+        }
+    } catch (err) {
         console.log(err);
     }
 })
